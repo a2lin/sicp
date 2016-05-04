0779a1d7dd60d2acabdb1184110c6a238d6138fa
Alexander Lin <lin.alexander.2011@gmail.com>
2016-05-03 23:36:53 -0700

    2.62

---
+++ Ch2/2_62.scm
@@ -0,0 +1,11 @@
+(load "2_61.scm")
+(define (union-set set1 set2)
+  (cond ((and (null? set1) (null? set2)) '())
+        ((null? set1) set2)
+        ((null? set2) set1)
+        ((< (car set1) (car set2)) (cons (car set1) (union-set (cdr set1) set2)))
+        ((= (car set1) (car set2)) (cons (car set1) (union-set (cdr set1) (cdr set2))))
+        ((> (car set1) (car set2)) (cons (car set2) (union-set set1 (cdr set2))))
+        ))
+
+(union-set (list 1 3 5 7 9) (list 2 4 6 8 10))
