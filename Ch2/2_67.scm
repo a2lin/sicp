3eb604d38a1e91e411489ebc6efb9a584714960c
Alexander Lin <lin.alexander.2011@gmail.com>
2016-05-08 23:17:42 -0700

    2.67a

--- Ch2/2_67.scm
+++ Ch2/2_67.scm
@@ -4,25 +4,21 @@
   (eq? (car object) 'leaf))
 (define (symbol-leaf x) (cadr x))
 (define (weight-leaf x) (caddr x))
-
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+    (list (symbol-leaf tree))
+    (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+    (weight-leaf tree)
+    (cadddr tree)))
 (define (make-code-tree left right)
   (list left
         right
         (append (symbols left) (symbols right))
         (+ (weight left) (weight right))))
-
-(define (left-branch tree) (car tree))
-
-(define (right-branch tree) (cadr tree))
-(define (symbols tree)
-  (if (leaf? tree)
-    (weight-leaf tree)
-    (cadddr tree)))
-(define (weight tree)
-  (if (leaf? tree)
-    (weight-leaf tree)
-    (cadddr tree)))
-
 (define (decode bits tree)
   (define (decode-1 bits current-branch)
     (if (null? bits)
@@ -61,3 +57,6 @@
                     (make-code-tree (make-leaf 'D 1)
                                     (make-leaf 'C 1)))))
 (define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+;(decode sample-message sample-tree)
+;adabbca
