21858e21159ed2e72a3f9292d22baccc0c776589
Alexander Lin <lin.alexander.2011@gmail.com>
2016-05-10 00:18:34 -0700

    using adjoin instead of custom rolling code >_>

--- Ch2/2_69.scm
+++ Ch2/2_69.scm
@@ -4,16 +4,11 @@
   (successive-merge (make-leaf-set pairs)))
 
 (define (successive-merge pairs)
-  (define (insert alist pair)
-    (cond ((null? alist) (list pair))
-          ((< (weight (car alist)) (weight pair)) (cons (car alist) (insert (cdr alist) pair)))
-          (else (cons pair alist))
-          ))
   (if (null? (cdr pairs))
     pairs
     (let ((left (car pairs))
           (right (cadr pairs)))
       (let ((tree (make-code-tree left right)))
-        (successive-merge (insert (cddr pairs) tree))))))
-
+        (successive-merge (adjoin-set tree (cddr pairs)))
+        ))))
 ;(successive-merge (make-leaf-set (list (list 'A 4) (list 'B 2) (list 'C 1) (list 'D 1))))
