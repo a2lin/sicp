a4d08facf88e0bcc6149803208e1ef46ecca28d9
Alexander Lin <lin.alexander.2011@gmail.com>
2016-05-11 22:11:01 -0700

    2.73

---
+++ Ch2/2_73.scm
@@ -0,0 +1,8 @@
+; a. because number? and same-variable? are the base cases for the recursion.
+;    if they were eaten into the data-directed dispatch we'd have an infinite
+;    recursion.
+
+; b.
+
+(define deriv-sum
+
