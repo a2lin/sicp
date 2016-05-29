fe62f1f0841d9d35a3e881b74fc4a34155923042
Alexander Lin <lin.alexander.2011@gmail.com>
2016-05-28 23:46:24 -0700

    3.3 actual

--- Ch3/3_3.scm
+++ Ch3/3_3.scm
@@ -1,2 +1,18 @@
-; placeholder
-
+(define (make-account balance password password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient Funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch m user-password)
+    (if (eq? password user-password) 
+      (cond ((eq? m 'withdraw) withdraw)
+            ((eq? m 'deposit) deposit)
+            (else (error "Unknown request -- MAKE ACCOUNT"
+                         m)))
+      "INCORRECT PASSWORD"))
+  dispatch)
+    
