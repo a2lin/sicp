0ee144d70f57bf14f5930c780f33d71ba882f04e
Alexander Lin <lin.alexander.2011@gmail.com>
2016-06-04 23:48:19 -0700

    3.9r

---
+++ Ch3/3_9.scm
@@ -0,0 +1,10 @@
+; factorial -v
+    ;6 -v
+        ;5 -v
+            ;4 -v
+                ;3 -v
+                    ;2 -v
+                        ;1 -v
+                            ;0 -v
+; factorial -v
+    ;exec
