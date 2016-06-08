d7d3d9a9727d076de047d0a7fed26cf9bc616770
Alexander Lin <lin.alexander.2011@gmail.com>
2016-06-07 23:38:16 -0700

    3.14

---
+++ Ch3/3_14.scm
@@ -0,0 +1,6 @@
+; this procedure reverses the list
+
+; winds up with something like [d][=>][c][=>][b][=>][a][=>][][\]
+;                                                    ^--v
+
+; I would expect v -> list('a), and w -> list('d 'c 'b 'a)
