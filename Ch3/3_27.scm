b5b2430fc85dcb67198e2a9b1782255bfa0de9ee
Alexander Lin <lin.alexander.2011@gmail.com>
2016-06-24 23:52:01 -0700

    3.27

---
+++ Ch3/3_27.scm
@@ -0,0 +1,14 @@
+; environment diagram ->
+
+; memoize
+; let -> table
+; -> lambda
+;    -> compute result and shove in table
+
+; continue to use the let
+; calculate results.
+
+
+; memo-fib stores the last computed element so it does not need to recomput the elements.
+; this would most certainly not work, because different tables would be used.
+
