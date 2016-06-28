9c3744ad259402aa6e8815fbc41c26359beda136
Alexander Lin <lin.alexander.2011@gmail.com>
2016-06-28 08:42:06 -0700

    3.31

---
+++ Ch3/3_31.scm
@@ -0,0 +1,5 @@
+; propagate does nothing if there is nothing in the agenda
+; the agenda is populated by (after-delay)
+; (after-delay) is in the procedure
+; therefore we need to call the procedure to put stuff in the agenda
+; to have anything to propagate.
