a5b2557226aa9cf20f860555c26949f08bd768da
Alexander Lin <lin.alexander.2011@gmail.com>
2016-06-29 00:28:05 -0700

    3.32

--- Ch3/3_32.scm
+++ Ch3/3_32.scm
@@ -1,2 +1,12 @@
 ; evidently lifo != fifo
-; and 
+; since the 'actions' are SETs on the final wire value, we have
+; the following:
+
+; start with (0, 1)
+; next step -> first wire goes to 1.
+; 1&1 = 1, so put 1 into the agenda for t->and-delay
+; step after that -> second wire goes to 0.
+; we have 1&0 -> second wire goes to 0, so put 0 into the agenda for t->and-delay.
+
+; if we do lifo, we will see 1.
+; if we do fifo, we will see 0.
