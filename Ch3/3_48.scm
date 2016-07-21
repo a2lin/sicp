309ddeb0a3b682adfee84ccb6d4c823d192e3333
Alexander Lin <lin.alexander.2011@gmail.com>
2016-07-20 23:58:30 -0700

    3.48

--- Ch3/3_48.scm
+++ Ch3/3_48.scm
@@ -6,3 +6,39 @@
 ; resources you must have consumed the priors.
 
 
+(define (make-account-and-serializer balance) 
+  (let ((number-serializer (make-serializer))
+        (number 0))
+    (define (withdraw amount)
+      (if (>= balance amount)
+        (begin (set! balance (- balance amount)) balance) "Insufficient funds"))
+    (define (deposit amount)
+      (set! balance (+ balance amount)) balance)
+
+    (define (get-number)
+      (let ((old-number number))
+        (set! number (+ 1 number))
+        old-number))
+
+    (let ((balance-serializer (make-serializer))
+          (acct-number (number-serializer get-number)))
+      (define (dispatch m)
+        (cond ((eq? m 'withdraw) (balance-serializer withdraw)) 
+              ((eq? m 'deposit) (balance-serializer deposit)) 
+              ((eq? m 'balance) balance)
+              ((eq? m 'serializer) balance-serializer)
+              ((eq? m 'number) number)
+              (else (error "Unknown request: MAKE-ACCOUNT" m)))) 
+      dispatch)))
+
+
+(define (serialized-exchange account1 account2)
+  (define (exchange less1 less2)
+    (let ((serializer1 (less1 'serializer))
+          (serializer2 (less2 'serializer)))
+      ((serializer1 (serializer2 exchange))
+       account1
+       account2)))
+  (if (< (account1 'number) (account2 'number))
+    (exchange account1 account2)
+    (exchange account2 account1)))
