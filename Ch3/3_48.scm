31d8a079ba27e49641e83657563b0bccbb2ab468
Alexander Lin <lin.alexander.2011@gmail.com>
2016-07-20 23:49:00 -0700

    3.48 text

---
+++ Ch3/3_48.scm
@@ -0,0 +1,8 @@
+; this prevents the dual-sided acquisition problem
+; by enforcing a strict acquisition order. It can 
+; never be that you and another process want
+; the same resources and both processes have
+; parts of the resources since to get the later
+; resources you must have consumed the priors.
+
+
