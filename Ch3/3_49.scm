51e749fbd8b466d50fae62b3275bc82a9c060b11
Alexander Lin <lin.alexander.2011@gmail.com>
2016-07-21 23:54:36 -0700

    3.49 stab

---
+++ Ch3/3_49.scm
@@ -0,0 +1,3 @@
+; this deadlock resolution strategy does not work well in the case that it is possible for different processes to grab different locks.
+; we need to ensure consistent numbering across strategies.
+; if it is impossible to ensure this consistent numbering, then this deadlock resolution strategy does not work.
