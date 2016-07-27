00c74da6679316735ca97c4f64a766ce5b28e798
Alexander Lin <lin.alexander.2011@gmail.com>
2016-07-26 23:26:38 -0700

    3.54, but streams broken

--- Ch3/stream_lib.scm
+++ Ch3/stream_lib.scm
@@ -1,3 +1,10 @@
+(define (cons-stream a b)
+  (cons a (delay b)))
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+(define the-empty-stream '())
+(define stream-null? null?)
+
 (define (stream-map . argstreams)
     (if (stream-null? (car argstreams))
       the-empty-stream
@@ -11,31 +18,28 @@
     (stream-car s)
     (stream-ref (stream-cdr s) (- n 1))))
 
-(define (stream-car stream) (car stream))
-(define (stream-cdr stream) (force (cdr stream)))
-(define the-empty-stream '())
-(define stream-null? null?)
-(define (cons-stream a b)
-  (cons a (delay b)))
-
 (define (stream-enumerate-interval low high)
   (if (> low high)
     the-empty-stream
     (cons-stream
       low
       (stream-enumerate-interval (+ low 1) high))))
+
 (define (display-line x)
   (display x)
   (newline))
+
 (define (show x)
   (display-line x)
   x)
+
 (define (stream-for-each proc s) 
   (if (stream-null? s)
     'done
     (begin (proc (stream-car s))
            (stream-for-each proc (stream-cdr s)))))
+
 (define (display-stream s) (stream-for-each display-line s))
-(define (add-streams s1 s2) (stream-map + s1 s2))
-(define (integers) (cons-stream 1 (add-streams ones integers)))
 (define (ones) (cons-stream 1 ones))
+(define (integers) (cons-stream 1 (add-streams (ones) integers)))
+(define (add-streams s1 s2) (stream-map + s1 s2))
