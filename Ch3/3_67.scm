6427180a95dcd5f528c02da568504439907229fe
Alexander Lin <lin.alexander.2011@gmail.com>
2016-08-13 23:20:31 -0700

    3.67 - fixed

--- Ch3/3_67.scm
+++ Ch3/3_67.scm
@@ -3,6 +3,9 @@
   (cons-stream
     (list (stream-car s) (stream-car t))
     (interleave
-      (stream-map (lambda (x) (list (stream-car s) x))
-                  (stream-cdr t))
+      (interleave
+        (stream-map (lambda (x) (list (stream-car s) x))
+                    (stream-cdr t))
+        (stream-map (lambda (x) (list x (stream-car s))
+                      (stream-cdr t))))
       (pairs (stream-cdr s) (stream-cdr t)))))
