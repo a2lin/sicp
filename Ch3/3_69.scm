9dd73dd3e59532219e2e3ed8eb7ed35ccb791264
Alexander Lin <lin.alexander.2011@gmail.com>
2016-08-16 00:04:00 -0700

    3.69 - not run

--- Ch3/3_69.scm
+++ Ch3/3_69.scm
@@ -1,5 +1,9 @@
 (define (triples s t u)
-  (stream-cons (list (stream-car s) (stream-car t) (stream-car u))
-               (interleave
-                 (stream-map (lambda (x y) (list (stream-car s) x y)) stream-cdr 
-                 (triples s (stream-cdr t) (stream-cdr u)))))
+  (cons-stream
+    (list (stream-car s) (stream-car t) (stream-car u))
+    (interleave
+        (interleave
+          (stream-map (lambda (x) (list (stream-car s) (stream-car t) x))
+                        (stream-cdr u))
+          (triples s (stream-cdr t) (stream-cdr u)))
+        (triples (stream-cdr s) (stream-cdr t) (stream-cdr u)))))
