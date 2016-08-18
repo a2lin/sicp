299217102f94a041a3fbde85b4096019771c3853
Alexander Lin <lin.alexander.2011@gmail.com>
2016-08-17 23:41:38 -0700

    3.70a

--- Ch3/3_70.scm
+++ Ch3/3_70.scm
@@ -1,3 +1,5 @@
+(load "stream_lib.scm")
+
 (define (merge s1 s2)
   (cond ((stream-null? s1) s2)
         ((stream-null? s2) s1)
@@ -27,15 +29,36 @@
             (cond ((< (weight s1car) (weight s2car))
                    (cons-stream
                      s1car
-                     (merge (stream-cdr s1) s2)))
+                     (merge-weighted weight (stream-cdr s1) s2)))
                   ((> (weight s1car) (weight s2car))
                    (cons-stream
                      s2car
-                     (merge s1 (stream-cdr s2))))
+                     (merge-weighted weight s1 (stream-cdr s2))))
                   (else
                     (cons-stream
                       s1car
                       (cons-stream
                         s2car
-                        (merge (stream-cdr s1)
+                        (merge-weighted weight (stream-cdr s1)
                              (stream-cdr s2))))))))))
+
+(define (pairs s t)
+  (cons-stream
+    (list (stream-car s) (stream-car t))
+    (interleave
+      (stream-map (lambda (x) (list (stream-car s) x))
+                  (stream-cdr t))
+      (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (weighted-pairs s t weight)
+  (cons-stream
+    (list (stream-car s) (stream-car t))
+    (merge-weighted weight
+      (merge-weighted weight
+        (stream-map (lambda (x) (list (stream-car s) x))
+                    (stream-cdr t))
+        (stream-map (lambda (x) (list x (stream-car s)))
+                    (stream-cdr t)))
+      (pairs (stream-cdr s) (stream-cdr t)))))
+
+(take (weighted-pairs (integers) (integers) (lambda (x) (+ (car x) (cadr x)))) 10)
