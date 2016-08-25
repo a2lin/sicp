d6cbea85d418bd88c0b9544e640f74e3a28abc46
Alexander Lin <lin.alexander.2011@gmail.com>
2016-08-24 22:59:03 -0700

    3_74.scm

---
+++ Ch3/3_74.scm
@@ -0,0 +1,17 @@
+(load "stream_lib.scm")
+
+;(define (make-zero-crossings input-stream last-value)
+;  (cons-stream
+;    (sign-change-detector
+;      (stream-car input-stream)
+;      last-value)
+;    (make-zero-crossings
+;      (stream-cdr input-stream)
+;      (stream-car input-stream))))
+;(define zero-crossings
+;  (make-zero-crossings sense-data 0))
+
+(define zero-crossings
+  (stream-map sign-change-detector
+              sense-data
+              (stream-cdr sense-data)))  
