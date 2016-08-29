b8fdaaa2761b94c56af7c957154dcf27bbee2bd2
Alexander Lin <lin.alexander.2011@gmail.com>
2016-08-28 23:41:15 -0700

    3.77 INCOMPLETE

---
+++ Ch3/3_77.scm
@@ -0,0 +1,9 @@
+(define (integral integrand initial-value dt)
+  (cons-stream
+    initial-value
+    (if (stream-null? integrand)
+      '()
+      (integral (stream-cdr integrand)
+                (+ (* dt (stream-car integrand))
+                   initial-value)
+                dt))))
