996d4ef8f5e07b69c0abcff6c550e7d08054026f
Alexander Lin <lin.alexander.2011@gmail.com>
2016-08-30 23:46:46 -0700

    3.77

--- Ch3/3_77.scm
+++ Ch3/3_77.scm
@@ -1,9 +1,12 @@
-(define (integral integrand initial-value dt)
+(define (integral delayed-integrand initial-value dt)
   (cons-stream
     initial-value
-    (if (stream-null? integrand)
-      '()
-      (integral (stream-cdr integrand)
-                (+ (* dt (stream-car integrand))
-                   initial-value)
-                dt))))
+    (let ((integrand (force delayed-integrand)))
+      (if (stream-null? integrand)
+        '()
+        (integral (delay stream-cdr integrand)
+                  (+ (* dt (stream-car integrand))
+                     initial-value)
+                  dt))))
+; 'every caller of integral must now `delay` the integrand argument.
+; this includes us!
