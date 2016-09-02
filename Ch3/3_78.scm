93b8cc2d95471784125b9651fdd894ef9619f8fc
Alexander Lin <lin.alexander.2011@gmail.com>
2016-09-01 23:54:56 -0700

    3.78 no-delay (nonwork)

--- Ch3/3_78.scm
+++ Ch3/3_78.scm
@@ -1,4 +1,15 @@
 (load "stream_lib.scm")
+
+; start -> map:f -> dy -> integral (y0) -> y -> start
+(define (solve f y0 dt)
+  (define y (integral dy y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+
 (define (solve-2nd a b dt y0 dy0)
-  (integral
-    (integral dy0
+  (define ddy (stream-add (stream-scale y)
+                        (stream-scale dy)))
+  (define dy (integral dy0 ddy))
+  (define y (integral y0 dy))
+  y)
