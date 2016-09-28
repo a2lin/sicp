c98b51a874531a11a819c7625375116d28e439d5
Alexander Lin <lin.alexander.2011@gmail.com>
2016-09-27 23:22:30 -0700

    3.77 tested

--- Ch3/3_77.scm
+++ Ch3/3_77.scm
@@ -1,12 +1,33 @@
+(load "stream_lib.scm")
 (define (integral delayed-integrand initial-value dt)
   (cons-stream
     initial-value
     (let ((integrand (force delayed-integrand)))
       (if (stream-null? integrand)
         '()
-        (integral (delay stream-cdr integrand)
+        (integral (delay (stream-cdr integrand))
                   (+ (* dt (stream-car integrand))
                      initial-value)
                   dt))))
+  )
 ; 'every caller of integral must now `delay` the integrand argument.
 ; this includes us!
+
+(define (integral-ref delayed-integrand initial-value dt)
+  (define int
+    (cons-stream
+      initial-value
+      (let ((integrand (force delayed-integrand)))
+        (add-streams (scale-stream integrand dt) int))))
+    int)
+              
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+;(stream-ref (solve (lambda (y) y)
+;                   1
+;                   0.0001)
+;            10000)
