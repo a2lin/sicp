61e2cb57f49ba31e88db5ec67769b99edffcdacd
Alexander Lin <lin.alexander.2011@gmail.com>
2016-09-29 23:46:16 -0700

    broken 3.78

--- Ch3/3_78.scm
+++ Ch3/3_78.scm
@@ -2,14 +2,16 @@
 
 ; start -> map:f -> dy -> integral (y0) -> y -> start
 (define (solve f y0 dt)
-  (define y (integral dy y0 dt))
+  (define y (integral (delay dy) y0 dt))
   (define dy (stream-map f y))
   y)
 
 
 (define (solve-2nd a b dt y0 dy0)
-  (define ddy (stream-add (stream-scale y)
-                        (stream-scale dy)))
+  (define ddy (stream-add (scale-stream (delay y) a)
+                        (scale-stream (delay dy) b)))
   (define dy (integral dy0 ddy))
   (define y (integral y0 dy))
   y)
+
+(solve-2nd 1 3 0.0001 3 7)
