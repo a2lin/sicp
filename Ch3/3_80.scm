eb3c89c922f74323730a3d3604cc0b12bb14c0ff
Alexander Lin <lin.alexander.2011@gmail.com>
2016-11-10 23:27:21 -0800

    added a case that should return something

--- Ch3/3_80.scm
+++ Ch3/3_80.scm
@@ -20,7 +20,10 @@
 ;    |--------------[scale: -R/L]<----|
 
 (define (RLC R L C Vc0 iL0 dt)
-  (define Vc (integral (scale-stream (delay iL) (/ -1 C)) Vc0 dt))
-  (define iL (integral (add-streams (scale-stream (Vc) (/ 1 L)) (scale-stream (delay iL) (/ (* -1 R) L))) iL0 dt))
-  (cons Vc iL)
+  (define iL (integral (add-streams (scale-stream (delay Vc) (/ 1 L)) (scale-stream (delay iL) (/ (* -1 R) L))) iL0 dt))
+  (define Vc (integral (scale-stream (iL) (/ -1 C)) Vc0 dt))
+  (define state (stream-map cons iL Vc))
+  state
   )
+
+(take (RLC 1000 1 0.001 1 1 0.001) 1)
