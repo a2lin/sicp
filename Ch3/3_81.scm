955578072c16a3071f33b98e123ace243edcc426
Alexander Lin <lin.alexander.2011@gmail.com>
2016-11-12 00:44:40 -0800

    3.81 illus

--- Ch3/3_81.scm
+++ Ch3/3_81.scm
@@ -1,7 +1,12 @@
 (load "stream_lib.scm")
+(define (rand-update x)
+  (modulo (+ (* x 1103515245) 12345) 32768))
+
+(define random-init 0)
+
 (define random-numbers
   (cons-stream
     random-init
     (stream-map rand-update random-numbers)))
 
-(define random-number-stream request-stream rand0)
+(take random-numbers 7)
