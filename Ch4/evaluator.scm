dde1846b3357c701b20c3969730ad11a09fffa41
Alexander Lin <lin.alexander.2011@gmail.com>
2016-11-29 22:52:45 -0800

    formatting

--- Ch4/evaluator.scm
+++ Ch4/evaluator.scm
@@ -242,7 +242,7 @@
       (cond ((null? vars)
              (add-binding-to-frame! var val frame))
             ((eq? var (car vars)) (set-car! vals val))
-      (else (scan (cdr vars) (cdr vals)))))
+            (else (scan (cdr vars) (cdr vals)))))
   (scan (frame-variables frame) (frame-values frame))))
 
 
