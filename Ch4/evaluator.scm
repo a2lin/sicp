1caf6ededf6957df3de48d039d64b891aaa26fbf
Alexander Lin <lin.alexander.2011@gmail.com>
2016-12-02 21:37:21 -0800

    env

--- Ch4/evaluator.scm
+++ Ch4/evaluator.scm
@@ -245,4 +245,12 @@
             (else (scan (cdr vars) (cdr vals)))))
   (scan (frame-variables frame) (frame-values frame))))
 
-
+(define (setup-environment)
+  (let ((initial-env
+          (extend-environment (primitive-procedure-names)
+                              (primitive-procedure-objects)
+                              the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+(define the-global-environment (setup-environment))
