81cb0cd30d0e31f5d9873d389505309ca6359ab7
Alexander Lin <lin.alexander.2011@gmail.com>
2016-12-03 22:49:05 -0800

    primitive procs

--- Ch4/evaluator.scm
+++ Ch4/evaluator.scm
@@ -254,3 +254,22 @@
     (define-variable! 'false false initial-env)
     initial-env))
 (define the-global-environment (setup-environment))
+
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+
+(define (primitive-implementation proc) (cadr proc))
+
+(define primitive-procedures
+  (list (list 'car car)
+        (list 'cdr cdr)
+        (list 'cons cons)
+        (list 'null null?)))
+
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+
+(define (primitive-procedure-objects)
+  (map (lambda (proc) (list 'primitive (cadr proc)))
+       primitive-procedures))
+        
