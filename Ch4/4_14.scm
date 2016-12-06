3ef977461d187c3cafd4b94d0047241a8cec9208
Alexander Lin <lin.alexander.2011@gmail.com>
2016-12-05 23:25:41 -0800

    4.14

---
+++ Ch4/4_14.scm
@@ -0,0 +1,5 @@
+;Louis's map fails even though Eva's works because the normal 'map' 
+;does not evaluate the primitives like a defined 'map' will.
+;Mixing the two evaluation strategies will result in problems
+;as the real evaluator and the interpreted evaluator do not
+;communicate this state to each other.
