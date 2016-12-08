8c1afd4294a8d1c17251794bf2d6f7e47f570606
Alexander Lin <lin.alexander.2011@gmail.com>
2016-12-07 23:42:33 -0800

    4.16

---
+++ Ch4/4_16.scm
@@ -0,0 +1,15 @@
+; 
+;(define (f x)
+;  (define (even? n) 
+;    (if (= n 0) 
+;      true 
+;      (odd? (- n 1)))) 
+;  (define (odd? n) 
+;    (if (= n 0) 
+;      false 
+;      (even? (- n 1)))) 
+;  ⟨rest of body of f⟩)
+;
+; Design mechanism to make this valid:: both need
+; to be added at the same time
+
