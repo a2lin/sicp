96b8ca67ec0d9be1f3c6161141e3b388ee9bca6e
Alexander Lin <lin.alexander.2011@gmail.com>
2016-12-08 23:20:57 -0800

    4.16a

--- Ch4/4_16.scm
+++ Ch4/4_16.scm
@@ -13,3 +13,19 @@
 ; Design mechanism to make this valid:: both need
 ; to be added at the same time
 
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars) 
+             (env-loop (enclosing-environment env))) 
+            ((eq? var (car vars)) 
+             (if (eq? (car vals) '*UNASSIGNED*)
+               (error "unassigned")
+               (car vals)))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+      (error "Unbound variable" var)
+      (let ((frame (first-frame env)))
+        (scan (frame-variables frame)
+              (frame-values frame)))))
+  (env-loop env))
