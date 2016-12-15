037a1407af6a94c7808c06752ac98775cef8f68d
Alexander Lin <lin.alexander.2011@gmail.com>
2016-12-14 22:46:06 -0800

    4.18 x

---
+++ Ch4/4_18.scm
@@ -0,0 +1,6 @@
+; this is going to cause problems because a and b 
+; depend on each other, and the let statements are 
+; themselves transformed.
+
+; if scanned out in the way the text showed,
+; a+b will transform correctly w.r.t each other.
