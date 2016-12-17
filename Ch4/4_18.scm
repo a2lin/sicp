8a7ebcd7f72b44814ab734cf6763fffb678ca29a
Alexander Lin <lin.alexander.2011@gmail.com>
2016-12-16 23:37:53 -0800

    4.18 actual

--- Ch4/4_18.scm
+++ Ch4/4_18.scm
@@ -1,46 +1,52 @@
-; this is going to cause problems because a and b 
-; depend on each other, and the let statements are 
-; themselves transformed.
+; we can show that the case works in the first case but is a problem in the
+; second case by transforming the let into lambdas.
 
-; if scanned out in the way the text showed,
-; a+b will transform correctly w.r.t each other.
+(define (solve f y0 dt)
+  (let ((y '*unassigned*)
+        (dy '*unassigned*))
+    (set! y (integral (delay dy) y0 dt))
+    (set! dy ((stream-map f y)))
+    y))
+; xform this into:
+(define (solve f y0 dt)
+  ((lambda (y dy)
+     (set! y (integral (delay dy) y0 dt))
+     (set! dy ((stream-map f y)))
+     y)
+   '*unassigned*
+   '*unassigned*))
 
-; okay let's do this the right way (e.g. actually try to do it right):
-; Scanning out the first:
-; (define (solve f y0 dt)
-;   (lambda <vars>
-;     (let ((y '*unassigned*) (dy '*unassigned*))
-;       (let ((a (integral (delay dy) y0 dt))
-;             (b (stream-map f y)))
-;             (set! y a)
-;             (set! dy b))
-;        y
-;     )
-;   )
-; )
-; 
-; second transformation:
-; ((lambda y dy)
-;    (lambda (a b)
-;       set! y a
-;       set! dy b
-;    integral (delay dy) y0 dt
-;    stream-map f y
-;   y
-; 'unassigned
-; 'unassigned
-; and therefore we are going to have a problem.
+(define (solve f y0 dt)
+  (let ((y '*unassigned*) (dy '*unassigned*))
+    (let ((a (integral (delay dy) y0 dt)) (b (stream-map f y)))
+      (set! y a)
+      (set! dy b)
+      y)))
+; xform into lambda
+(define (solve f y0 dt)
+  ((lambda (y dy)
+     (let ((a (integral (delay dy) y0 dt)) (b (stream-map f y)))
+       (set! y a)
+       (set! dy b)
+       y))
+   '*unassigned*
+   '*unassigned*))
+; we can see that y, dy are '*unassigned' value-wise, but this gets fixed before
+; the lambda returns.
 
-; in the normal version,
-; it expands out to look like
-;(define (solve f y0 dt)
-;  ((lambda <vars>
-;    (let ((y '*unassigned*)
-;          (dy '*unassigned*))
-;      (set! y (integral (delay dy) y0 dt))
-;      (set! dy ((stream-map f y)))
-;      y))
-;     '*unassigned*
-;     '*unassigned*
+; xform other into lambda
+(define (solve f y0 dt)
+  ((lambda (y dy)
+     ((lambda (a b)
+        (set! y a)
+        (set! dy b)
+        y)
+      (integral (delay dy) y0 dt)
+      (stream-map f y))
+   '*unassigned*
+   '*unassigned*)))
 
-; in this event this should be fine as both names available and set.
+; dy, y are both 'unassigned; so
+; (integral (delay '*unassigned*) y0 dt)
+; (stream-map f '*unassigned*)
+; and therefore we have a problem.
