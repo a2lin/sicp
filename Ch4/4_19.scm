8b8616347c2c78fe1e28b65d8bd07a33b2c1bb88
Alexander Lin <lin.alexander.2011@gmail.com>
2016-12-17 23:52:25 -0800

    4.19

---
+++ Ch4/4_19.scm
@@ -0,0 +1,10 @@
+; Looking at this:
+; a = 1
+; f(x) = (b = 1 + x), (a = 5), (a + b) = (1 + 5 + x)
+; f(10) = (b = 1 + 10 = 11) (a = 5) (a + b) = 1 + 5 + 11 = 16
+
+; it seems natural that in a procedural language ben's point should hold
+
+; eva's point that the definitions should occur simultaneously as 
+; technically a, b being simultaneous is fine; we just
+; need to rewrite a and b definitions to the old 'let/lambda' model
